c8deeea87dd58b4b13d1a564fa4ad0a4de25898b
Olivier Barais <barais@irisa.fr>
2013-10-01 12:27:37 +0200

    initial commit

---
+++ src/main/java/fr/ensai/test8/App.java
@@ -0,0 +1,13 @@
+package fr.ensai.test8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
